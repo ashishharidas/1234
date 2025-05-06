75ed4b9fb817e897342dd968683acb921b4eb70c
ashishharidas <ashish101111@gmail.com>
2025-05-06 20:25:18 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -22,42 +22,44 @@ describe("Todolist Test Suite", () => {
       dueDate: new Date().toISOString().slice(0, 10),
     });
   });
-  test("add a  todo", () => {
+
+  test("Creating a new todo", () => {
     const todoItemCount = all.length;
 
     add({
-      title: "Test Todo",
+      title: "New Test Todo",
       completed: false,
       dueDate: new Date().toISOString().slice(0, 10),
     });
     expect(all.length).toBe(todoItemCount + 1);
   });
-  
-  test(" mark todo as complete", () => {
+
+  test("Should mark todo as complete", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
   });
-  test("Retrieve overdue items", () => {
+
+  test("Retrieval of overdue items", () => {
     const overdueItems = all.filter(
       (todo) =>
-        new Date(todo.dueDate) < new Date("2025-05-03") && !todo.completed,
+        new Date(todo.dueDate) < new Date() && !todo.completed
     );
     expect(overdueItems.length).toBe(0); // Adjusted to match the expected state of the 'all' array
   });
 
-  test("Retrieve due today items", () => {
+  test("Retrieval of due today items", () => {
     const dueTodayItems = all.filter(
       (todo) =>
         todo.dueDate === new Date().toISOString().slice(0, 10) &&
-        !todo.completed,
+        !todo.completed
     );
     expect(dueTodayItems.length).toBe(1); // Adjusted to match the expected state of the 'all' array
   });
 
-  test("Retrieve due later items", () => {
+  test("Retrieval of due later items", () => {
     const dueLaterItems = all.filter(
-      (todo) => new Date(todo.dueDate) > new Date() && !todo.completed,
+      (todo) => new Date(todo.dueDate) > new Date() && !todo.completed
     );
     expect(dueLaterItems.length).toBe(0);
   });
